1b90e235ca0af10913c4076a1ff636eaa34c8b13
Mateusz Czarczyński <48910943+Czarczynski@users.noreply.github.com>
2022-05-30 13:42:18 +0200

    feat(modal): haptic improved

--- src/features/modal/hooks/use-modal-height.tsx
+++ src/features/modal/hooks/use-modal-height.tsx
@@ -1,28 +1,18 @@
 import * as Haptics from 'expo-haptics'
 import { ImpactFeedbackStyle } from 'expo-haptics'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { GestureResponderHandlers, LayoutAnimation, PanResponder } from 'react-native'
-
-import useIsMount from '~hooks/use-is-mount'
 
 type useModalHeightType = [number, GestureResponderHandlers, () => void]
 
 export const useModalHeight = (onClose: () => void): useModalHeightType => {
   const [heightAnimValue, setHeightAnimValue] = useState(false)
   const [moveUsed, setMoveUsed] = useState(false)
-  const isMount = useIsMount()
 
-  useEffect(() => {
-    if (!isMount) {
-      return
-    }
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    Haptics.impactAsync(ImpactFeedbackStyle.Light)
-  })
-
-  const onDismiss = () => {
+  const onDismiss = async () => {
     onClose()
     setHeightAnimValue(false)
+    await Haptics.impactAsync(ImpactFeedbackStyle.Light)
   }
 
   const gestureResponders = useMemo(
@@ -35,20 +25,22 @@ export const useModalHeight = (onClose: () => void): useModalHeightType => {
         onPanResponderGrant: () => {
           setMoveUsed(false)
         },
-        onPanResponderMove: (e, gestureState) => {
+        onPanResponderMove: async (e, gestureState) => {
           if (gestureState.dy < -40 && !heightAnimValue) {
             LayoutAnimation.configureNext(LayoutAnimation.create(100, LayoutAnimation.Types.linear))
             setHeightAnimValue(true)
+            await Haptics.impactAsync(ImpactFeedbackStyle.Light)
           }
           if (gestureState.dy > 40 && heightAnimValue) {
             LayoutAnimation.configureNext(LayoutAnimation.create(100, LayoutAnimation.Types.linear))
             setHeightAnimValue(false)
+            await Haptics.impactAsync(ImpactFeedbackStyle.Light)
             setMoveUsed(true)
           }
         },
-        onPanResponderRelease: (e, gestureState) => {
+        onPanResponderRelease: async (e, gestureState) => {
           if (gestureState.dy > 40 && !heightAnimValue && !moveUsed) {
-            onDismiss()
+            await onDismiss()
           }
         },
       }),
