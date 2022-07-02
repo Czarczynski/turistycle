e444125504cd0782aa2e44bd88334073a34c3f83
Mateusz Czarczyński <48910943+Czarczynski@users.noreply.github.com>
2022-07-02 18:18:03 +0200

    feat(trip-preview): improved
    feat(chat): better performance & saving

--- src/features/ui/message-list/message-list.stories.tsx
+++ src/features/ui/message-list/message-list.stories.tsx
@@ -7,35 +7,10 @@ import { Message } from '~models/message.model'
 
 import { MessageList } from './message-list.component'
 
-const exampleData: Message[] = [
-  { text: 'Hello', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 13, 20)) },
-  { text: 'Hi', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14)) },
-  { text: 'Whats up', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14, 10)) },
-  { text: 'Great', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 18)) },
-  { text: 'Hello', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 13, 20)) },
-  { text: 'Hi', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14)) },
-  { text: 'Whats up', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14, 10)) },
-  { text: 'Great', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 18)) },
-  { text: 'Hello', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 13, 20)) },
-  { text: 'Hi', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14)) },
-  { text: 'Whats up', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14, 10)) },
-  { text: 'Great', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 18)) },
-  { text: 'Hello', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 13, 20)) },
-  { text: 'Hi', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14)) },
-  { text: 'Whats up', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14, 10)) },
-  { text: 'Great', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 18)) },
-  { text: 'Hello', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 13, 20)) },
-  { text: 'Hi', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14)) },
-  { text: 'Whats up', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14, 10)) },
-  { text: 'Great', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 18)) },
-  { text: 'Hello', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 13, 20)) },
-  { text: 'Hi', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14)) },
-  { text: 'Whats up', senderUid: '123', time: DateTime.fromJSDate(new Date(2022, 3, 20, 14, 10)) },
-  { text: 'Great', senderUid: '124', time: DateTime.fromJSDate(new Date(2022, 3, 20, 18)) },
-]
+const exampleData: Message[] = []
 
 storiesOf('MessageList', module)
   .addDecorator((getStory) => (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>{getStory()}</View>
   ))
-  .add('common', () => <MessageList userUid={'123'} data={exampleData} />)
+  .add('common', () => <MessageList data={exampleData} />)
