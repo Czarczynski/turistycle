76df8b6f9da0ef109b52c80b4ee5de1105d6b6cc
Mateusz Czarczyński <48910943+Czarczynski@users.noreply.github.com>
2022-07-14 15:18:32 +0200

    feat(eslint): fix lint

--- src/screens/chat/chat.styles.ts
+++ src/screens/chat/chat.styles.ts
@@ -13,10 +13,10 @@ export default StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: theme.text,
-  }, button: {
-    aspectRatio:1,
-    paddingRight:18,
-    paddingBottom:18,
-  }
-
+  },
+  button: {
+    aspectRatio: 1,
+    paddingRight: 18,
+    paddingBottom: 18,
+  },
 })
